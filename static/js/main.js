742f1d1ce1985e9408e6cb65e7034c9394604ff2
Henry-2016 <jacob2016henry@gmail.com>
2020-11-08 19:16:20 +0300

    New Kankan Client Uploads

---
+++ static/js/main.js
@@ -0,0 +1,13 @@
+
+// 192.168.43.23
+
+function LoadUsingGitHub ()
+{window.location='https://henry-2016.github.io/kankan-client-server-repo/'}
+
+function LoadUsingLapTop ()
+{window.location="http://192.168.43.23:80/kankan-client-server-repo/index.html"}
+
+function LoadUsingRaspberry ()
+{window.location="http://192.168.43.140:80/kankan-client-server-repo/index.html"}
+
+// https://henry-2016.github.io/kankan-admin-server-repo/. 
